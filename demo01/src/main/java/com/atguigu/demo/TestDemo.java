73f2bfe688f4c5bae33489444c5e5fe01dcb4731
zdychina <zdychina@sina.cn>
2019-07-11 10:04:39 +0800

    Initial commit

---
+++ demo01/src/main/java/com/atguigu/demo/TestDemo.java
@@ -0,0 +1,28 @@
+package com.atguigu.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author zdy
+ * @create 2019-07-09 21:19
+ */
+public class TestDemo {
+
+    public static void main(String[] args) {
+//        System.out.println("abc");
+//        System.out.println("abc");
+//        ArrayList list = new ArrayList();
+//        long maxMemory = Runtime.getRuntime().maxMemory();
+//        long totalMemory = Runtime.getRuntime().totalMemory();
+//
+//        System.out.println("maxMemory:"+maxMemory/(double)1024/1024+"M");
+//        System.out.println("totalMemory:"+totalMemory/(double)1024/1024+"M");
+
+        String str="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        while(true){
+            str+=str+new Random().nextInt(88888888) + new Random().nextInt(999999999) ;
+        }
+    }
+}
